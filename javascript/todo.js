11b5baef068f210632d3ca762713266695409ae1
mubin-bonu <mubin.me@mail.ru>
2022-11-29 22:50:01 +0600

    first commit

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,5 +1,4 @@
-// Здесь мы создаем кнопку "закрыть" и добавляем ее к каждому элементу списка
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
@@ -8,9 +7,7 @@ for (i = 0; i < myNodelist.length; i++) {
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
-
-// Нажмимаем на кнопку закрыть, чтобы скрыть текущий элемент списка
-var close = document.getElementsByClassName("close");
+var close = document.getElementsByClassName('close');
 var i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
@@ -18,38 +15,33 @@ for (i = 0; i < close.length; i++) {
         div.style.display = "none";
     };
 }
-
-// Добавляем символ "проверено" при нажатии на элемент списка
 var list = document.querySelector("ul");
 list.addEventListener(
     "click",
     function (ev) {
-        if (ev.target.tagName === "LI") {
-            ev.target.classList.toggle("checked");
+        if (ev.target.tagName ==="li") {
+            ev.target.classlist.toogle("checked");
         }
     },
     false
 );
 
-// Создаем новый элемент списка при нажатии на кнопку "Добавить"
 function newElement() {
-    var li = document.createElement("li");
+    var li = document.createElement("li")
     var inputValue = document.getElementById("myInput").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === "") {
         alert("Запишите Задачу!");
     } else {
-        document.getElementById("myUL").appendChild(li);
+        document.getElementById("myUl").appendChild(li);
     }
     document.getElementById("myInput").value = "";
-
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-
 
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function () {
